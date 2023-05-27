af4187e024d4f85cb7dd13514e4d818dc58f697f
Vatrala <vatralanaska@gmail.com>
2023-05-27 13:30:25 +0300

    basic react main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.scss';
-import Header from "./Components/Header/Header";
-import Banner from "./Components/Banner/Banner";
-import Main from "./Components/Main/Main";
 
 function App() {
 
     return (
         <>
-            <Header />
-            <Banner />
-            <Main />
         </>
     )
 }
