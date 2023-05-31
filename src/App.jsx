41778e6bc801d2a96c80d494cd3bb3d6c4a83264
Vatrala <vatralanaska@gmail.com>
2023-05-31 12:37:01 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss';
 import Header from "./Components/Header/Header";
 import Banner from "./Components/Banner/Banner";
 import Main from "./Components/Main/Main";
+import {UserForm} from "./Components/UserForm/UserForm";
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
         <>
             <Header />
             <Banner />
+            <UserForm />
             <Main />
+
         </>
     )
 }
